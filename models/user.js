b99a0b0355f38feb3ac5d0072c58234d497c184a
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-19 17:36:21 +0100

    hashear contraseña

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = new Sequelize(process.env.DB_URI, {
   dialect: 'postgres',
 });
@@ -11,9 +12,19 @@ const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     unique: true,
+    allowNull: false,
   },
   password: {
     type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+//Hashear la contraseña antes de guardar el usuario.
+User.beforeCreate(async (user, options) => {
+  if (user.password) {
+    const hashedPassword = await bcrypt.hash(user.password, 10); // Hashea la contraseña antes de guardarla
+    user.password = hashedPassword; // Asigna la contraseña hasheada al campo 'password'
   }
 });
 
