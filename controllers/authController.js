57f6ee6656e7d580c4199c578b012c17b461cc91
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-19 17:18:55 +0100

    modelsuser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const User = require('../models/user');
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
