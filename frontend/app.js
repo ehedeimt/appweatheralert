05f9b3bb20dc05e32ca9670be3250bfff358695a
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-19 13:56:37 +0100

    app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-document.getElementById('registerForm').addEventListener('submit', async (event) => {
+/*document.getElementById('registerForm').addEventListener('submit', async (event) => {
     event.preventDefault();
   
     const name = document.getElementById('name').value;
@@ -21,3 +21,34 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
       alert('Error al registrar usuario: ' + result.error);
     }
   });  
+*/
+
+
+
+  document.getElementById('registroForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    try {
+      const response = await fetch('/api/users/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log('Usuario registrado:', data);
+    } catch (error) {
+      console.error('Error al registrar usuario:', error);
+    }
+  });
+  
