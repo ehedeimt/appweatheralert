eba57e6d3509c8fcee99ecf6c1d50efcd61d6011
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-19 14:31:52 +0100

    quito comentarios app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,30 +1,3 @@
-/*document.getElementById('registerForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
-  
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-  
-    const response = await fetch('/api/users/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-  
-    const result = await response.json();
-    
-    if (response.ok) {
-      alert('Registro exitoso');
-    } else {
-      alert('Error al registrar usuario: ' + result.error);
-    }
-  });  
-*/
-
-
-
   document.getElementById('registroForm').addEventListener('submit', async function(event) {
     event.preventDefault();
   
