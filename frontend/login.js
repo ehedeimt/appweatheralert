c4e21af80609e393fed1a56b9d8760d1087fadf4
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-23 18:32:57 +0100

    actualización login.js para corregir error.

--- frontend/login.js
+++ frontend/login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const password = document.getElementById('password').value;
   
       try {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/auth/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
