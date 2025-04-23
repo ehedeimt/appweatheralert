d788479a8400d80a7f9a1fff4b73af98cafb4dbd
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-23 20:45:52 +0100

    styele

--- frontend/js/usuarioNombre.js
+++ frontend/js/usuarioNombre.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (token && nombreUsuario && contenedorUsuario && mensajeUsuario) {
     mensajeUsuario.textContent = `Bienvenido, ${nombreUsuario}`;
-    contenedorUsuario.style.visibility = "hidden";
+    contenedorUsuario.style.display = "flex";
   }
 
   if (cerrarSesionBtn) {
