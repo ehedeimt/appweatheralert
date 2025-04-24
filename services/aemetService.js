9708b30ef084dccc7867d4d386e12a7b2b8d8875
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-24 18:15:33 +0100

    añado aemetService.js

---
+++ services/aemetService.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const AEMET_API_KEY = process.env.AEMET_API_KEY;
+
+const obtenerAlertas = async () => {
+  try {
+    const res = await axios.get('https://opendata.aemet.es/opendata/api/avisos_cap/ccaa/', {
+      params: { api_key: AEMET_API_KEY }
+    });
+
+    const urlDatos = res.data.datos;
+    const datosAlertas = await axios.get(urlDatos);
+
+    return datosAlertas.data;
+  } catch (error) {
+    console.error('❌ Error al obtener alertas de AEMET:', error.message);
+    throw error;
+  }
+};
+
+module.exports = { obtenerAlertas };
