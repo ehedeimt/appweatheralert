3dd350d00154aadf361c947867df297967748f9c
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-05-05 21:07:05 +0100

    authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,8 @@
+/*
+authControler.js --> Autenticación del backend en Node.js permite el registro de usuarios, 
+login con el token generado y hasheaar la contraseña para permitir el login.
+*/
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
