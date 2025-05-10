7a0e29b9a527b784aa18815ea67f679724e6091d
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-05-10 20:38:09 +0100

    COMENTARIOS authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,3 +1,11 @@
+/*
+authroutes.js --> Rutas de autenticación para usuarios
+- POST /api/auth/register --> Registra un nuevo usuario.
+- POST /api/auth/login --> Inicia sesión y devuelve un token JWT.
+
+Estas dos rutas usan las funciones definidas en authcontroller.js
+*/
+
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
