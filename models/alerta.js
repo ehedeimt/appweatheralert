0e9566c57947e083b85e739a9e5c40434ee5977a
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-05-10 19:51:40 +0100

    COMENTARIOS models BBDD alerta y user

--- models/alerta.js
+++ models/alerta.js
@@ -1,8 +1,29 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_URI, {
+/*
+Modelo Sequelize para las alertas personalizadas creadas por los usuarios.
+
+Campos:
+- titulo: Nombre descriptivo de la alerta.
+- descripcion: Tipo o categoría de alerta (temperatura, playa, montaña...)
+- usuario_id: Referencia al usuario creador.
+- fecha_creacion: Fecha de creación (se inserta automáticamente).
+- municipio_id: Código de la zona geográfica asociada. Al añadir más alertas era necesario cambiar este nombre de columna ya que no siempre se guardan municipios.
+- dia_alerta_montana: Día específico para alertas de montaña (obligatorio para esta alerta, pero no para el resto y quedará nulo).
+
+Opciones:
+- Tabla asociada: 'alertas'
+
+Requiere:
+- Variable de entorno DB_URI con la URI de conexión a la BBDD PostgreSQL.
+*/
+
+
+//IMPORTACIONES Y CONFIGURACIONES DE SQUELIZE
+const { Sequelize, DataTypes } = require('sequelize');//importa el ORM Y los tipos de datos.
+const sequelize = new Sequelize(process.env.DB_URI, {//crea instancia de conexión con la BBDD creada en postgreSQL
   dialect: 'postgres',
 });
 
+//DEFINICIÓN DE COLUMNAS EN TABLA alertas.
 const Alerta = sequelize.define('Alerta', {
   titulo: {
     type: DataTypes.STRING,
--- models/user.js
+++ models/user.js
@@ -1,9 +1,25 @@
+/*
+Modelo Sequelize para usuarios.
+Campos:
+- name: Nombre completo del usuario.
+- email: Correo electrónico único.
+- password: Contraseña (se almacena encriptada)
+
+Seguridad:
+- Antes de crear un usuario, se hashea la contraseña con bcrypt.
+
+Requiere:
+- Variable de entorno DB_URI (PostgreSQL)
+ */
+
+//CONFIGURACIÓN E IMPORTACIONES.
 const { Sequelize, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = new Sequelize(process.env.DB_URI, {
+const bcrypt = require('bcrypt');//Para la encriptación o hasheo de la contraseña.
+const sequelize = new Sequelize(process.env.DB_URI, {//crea instancia de conexión con la BBDD creada en postgreSQL
   dialect: 'postgres',
 });
 
+//DEFINICIÓN DE CAMPOS DE LA TABLA User.
 const User = sequelize.define('User', {
   name: {
     type: DataTypes.STRING,
@@ -23,9 +39,9 @@ const User = sequelize.define('User', {
 //Hashear la contraseña antes de guardar el usuario.
 User.beforeCreate(async (user, options) => {
   if (user.password) {
-    const hashedPassword = await bcrypt.hash(user.password, 10); // Hashea la contraseña antes de guardarla
-    user.password = hashedPassword; // Asigna la contraseña hasheada al campo 'password'
+    const hashedPassword = await bcrypt.hash(user.password, 10); //Hashea la contraseña antes de guardarla
+    user.password = hashedPassword; // Asigna la contraseña hasheada al campo password
   }
 });
-
+//exporto para que pueda ser usado. por controladores externos como authController.js
 module.exports = User;
